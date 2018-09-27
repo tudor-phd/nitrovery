12371240ef66716bc7ad3a6d97b4dcb7ceb2adb8
Tudor <p_tudorr@yahoo.com>
2018-09-27 21:10:34 +0300

    Remove commented code from pipe.

--- my-app/src/app/filter.pipe.ts
+++ my-app/src/app/filter.pipe.ts
@@ -1,24 +1,3 @@
-// import { Pipe, PipeTransform } from '@angular/core';
-
-// @Pipe({
-//   name: 'filter'
-// })
-// export class FilterPipe implements PipeTransform {
-
-//   transform(items: any[], searchText: string): any[] {
-//     if (!items) return [];
-//     if (!searchText) return items;
-//     searchText = searchText.toLowerCase();
-//     return items.filter(it => {
-//       return it.cityOrigin.toString().toLowerCase().indexOf(searchText.toLowerCase()) > -1;
-//     });
-//   }
-
-// }
-
-
-
-
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
